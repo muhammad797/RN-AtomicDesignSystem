60f5a6f172ff1bc1f52958d2b54cf33958fd0324
Muhammad Ali <muhammad797@hotmail.com>
2022-12-29 16:14:19 +0500

    Typescript, ESLint, Prettier and Husky

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SafeAreaView, StatusBar } from 'react-native';
+
+const App = () => (
+  <SafeAreaView>
+    <StatusBar barStyle={'light-content'} />
+  </SafeAreaView>
+);
+
+export default App;
